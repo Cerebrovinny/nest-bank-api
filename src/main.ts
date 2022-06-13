012d0b2769ae065c0ad5292d2bdba9b37549175e
Cerebrovinny <vinny.cardoso96@gmail.com>
2022-06-12 22:11:01 -0300

    swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Bank API')
+    .setDescription('The Bank api')
+    .setVersion('1.0')
+    .addTag('Bank')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = 3000;
   await app.listen(3000);
+  console.log(`Server running on port http://localhost:${port}/api/#/`);
 }
 bootstrap();
